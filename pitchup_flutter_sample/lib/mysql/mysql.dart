c891f12979c8aba35784a8819298ca8963a02351
Kat Costales <kcostales25@my.whitworth.edu>
2023-05-16 21:53:56 -0700

    Editted UI Login and profile

--- pitchup_flutter_sample/lib/mysql/mysql.dart
+++ pitchup_flutter_sample/lib/mysql/mysql.dart
@@ -1,23 +1,23 @@
-import 'package:mysql1/mysql1.dart';
+// import 'package:mysql1/mysql1.dart';
 
-// https://www.youtube.com/watch?v=ig6WRq73iEg
-class Mysql {
-  static String host = 'melodyapp-1.c3wibgkxzweu.us-east-2.rds.amazonaws.com',
-                user = 'iteo24',
-                password = 'password',
-                db = 'melody-1';
-  static int port = 3306;
+// // https://www.youtube.com/watch?v=ig6WRq73iEg
+// class Mysql {
+//   static String host = 'melodyapp-1.c3wibgkxzweu.us-east-2.rds.amazonaws.com',
+//                 user = 'iteo24',
+//                 password = 'password',
+//                 db = 'melody-1';
+//   static int port = 3306;
 
-  Mysql();
+//   Mysql();
 
-  Future<MySqlConnection> getConnection() async {
-    var settings = ConnectionSettings(
-      host: host,
-      port: port,
-      user: user,
-      password: password,
-      db: db
-    );
-    return await MySqlConnection.connect(settings);
-  }
-}
+//   Future<MySqlConnection> getConnection() async {
+//     var settings = ConnectionSettings(
+//       host: host,
+//       port: port,
+//       user: user,
+//       password: password,
+//       db: db
+//     );
+//     return await MySqlConnection.connect(settings);
+//   }
+// }
