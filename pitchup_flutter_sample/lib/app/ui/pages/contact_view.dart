77e2ba1a8e2c9b13d1523398fef9f24690178178
Kat Costales <kcostales25@my.whitworth.edu>
2023-05-17 15:39:33 -0700

    added logo to landing page

--- pitchup_flutter_sample/lib/app/ui/pages/contact_view.dart
+++ pitchup_flutter_sample/lib/app/ui/pages/contact_view.dart
@@ -26,70 +26,70 @@ class ContactView extends StatelessWidget {
       )),
 
       // Contact Box - full name
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(12),
-          ), //Box Decoration
-          child: Padding(
-            padding: const EdgeInsets.only(left: 230.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Full Name',
-                hintStyle: TextStyle(color: Colors.black),
-              ), // InputDecoration
-            ), //TextField
-          ), //Padding
-        ), //Container
-      ), //Padding,
+      // child: Padding(
+      //   padding: const EdgeInsets.symmetric(horizontal: 40.0),
+      //   child: Container(
+      //     decoration: BoxDecoration(
+      //       color: Colors.grey[200],
+      //       border: Border.all(color: Colors.black),
+      //       borderRadius: BorderRadius.circular(12),
+      //     ), //Box Decoration
+      //     child: Padding(
+      //       padding: const EdgeInsets.only(left: 230.0),
+      //       child: TextField(
+      //         decoration: InputDecoration(
+      //           hintText: 'Full Name',
+      //           hintStyle: TextStyle(color: Colors.black),
+      //         ), // InputDecoration
+      //       ), //TextField
+      //     ), //Padding
+      //   ), //Container
+      // ), //Padding,
 
 
       // Phone Number Contact
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(12),
-          ), //Box Decoration
-          child: Padding(
-            padding: const EdgeInsets.only(left: 230.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Phone Number',
-                hintStyle: TextStyle(color: Colors.black),
-              ), // InputDecoration
-            ), //TextField
-          ), //Padding
-        ), //Container
-      ), //Padding,
+      // child: Padding(
+      //   padding: const EdgeInsets.symmetric(horizontal: 40.0),
+      //   child: Container(
+      //     decoration: BoxDecoration(
+      //       color: Colors.grey[200],
+      //       border: Border.all(color: Colors.black),
+      //       borderRadius: BorderRadius.circular(12),
+      //     ), //Box Decoration
+      //     child: Padding(
+      //       padding: const EdgeInsets.only(left: 230.0),
+      //       child: TextField(
+      //         decoration: InputDecoration(
+      //           hintText: 'Phone Number',
+      //           hintStyle: TextStyle(color: Colors.black),
+      //         ), // InputDecoration
+      //       ), //TextField
+      //     ), //Padding
+      //   ), //Container
+      // ), //Padding,
 
 
 
   // Email Contact
-  child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 40.0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[200],
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(12),
-          ), //Box Decoration
-          child: Padding(
-            padding: const EdgeInsets.only(left: 230.0),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                hintStyle: TextStyle(color: Colors.black),
-              ), // InputDecoration
-            ), //TextField
-          ), //Padding
-        ), //Container
-      ), //Padding,
+//   child: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal: 40.0),
+//         child: Container(
+//           decoration: BoxDecoration(
+//             color: Colors.grey[200],
+//             border: Border.all(color: Colors.black),
+//             borderRadius: BorderRadius.circular(12),
+//           ), //Box Decoration
+//           child: Padding(
+//             padding: const EdgeInsets.only(left: 230.0),
+//             child: TextField(
+//               decoration: InputDecoration(
+//                 hintText: 'Email',
+//                 hintStyle: TextStyle(color: Colors.black),
+//               ), // InputDecoration
+//             ), //TextField
+//           ), //Padding
+        // ), //Container
+      // ), //Padding,
       //  ), // Safe Area
     );
   }
